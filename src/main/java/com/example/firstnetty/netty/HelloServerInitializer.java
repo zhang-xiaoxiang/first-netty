b9ec57314c77df87d8d4273833e3b1981e3bf6a9
ZhangXiaoxiang <1090510535@qq.com>
2019-06-15 20:21:46 +0800

    Initial commit

---
+++ src/main/java/com/example/firstnetty/netty/HelloServerInitializer.java
@@ -0,0 +1,29 @@
+package com.example.firstnetty.netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ * HelloServerInitializer:初始化器，channel注册后，会执行里面的相应的初始化方法
+ * @author zhangxiaoxiang
+ * @date 2019/6/15
+ */
+public class HelloServerInitializer extends ChannelInitializer<SocketChannel> {
+
+	@Override
+	protected void initChannel(SocketChannel channel) throws Exception {
+		// 通过SocketChannel去获得对应的管道
+		ChannelPipeline pipeline = channel.pipeline();
+		
+		// 通过管道，添加handler
+		// HttpServerCodec是由netty自己提供的助手类，可以理解为拦截器
+		// 当请求到服务端，我们需要做解码，响应到客户端做编码
+		pipeline.addLast("HttpServerCodec", new HttpServerCodec());
+		
+		// 添加自定义的助手类，返回 "hello netty~"
+		pipeline.addLast("customHandler", new CustomHandler());
+	}
+
+}
